8d06242767cd4d3fe55a65dcbfddc80b38ddbe30
rolandmontalvan <rolandfilho@gmail.com>
2021-04-13 10:45:28 -0300

    atualizaçao do gitignore

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ApiService } from './api.service';
+import { ShoppingItem } from './shopping-item.interface.js';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div style="text-align:center">
+    <h1>
+      Lista de compras
+    </h1>
+  </div>
+  <ul>
+    <li *ngFor="let item of items">
+      <h2>{{ item.name }}</h2>
+    </li>
+  </ul>
+  {{ error?.message }}
+  `
+})
+export class AppComponent implements OnInit {
+
+  items: ShoppingItem[];
+  error: any;
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit() {
+    this.api.getShoppingItems().subscribe(
+      (items: ShoppingItem[]) => this.items = items,
+      (error: any) => this.error = error
+    );
+  }
+}
